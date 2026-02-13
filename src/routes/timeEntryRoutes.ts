b610dd2c36bfdb7d2b2992ca239252b9ebec772e
mehfuz-enacton <mhshaikh@enacton.email>
2026-02-13 10:13:57 +0530

    Format code

--- src/routes/timeEntryRoutes.ts
+++ src/routes/timeEntryRoutes.ts
@@ -14,19 +14,10 @@ const router = Router();
 
 router.use(protect);
 
-// Get time entries by project
 router.get("/project/:projectId", getTimeEntriesByProject);
-
-// Get single time entry by id
 router.get("/:id", getTimeEntryById);
-
-// Create time entry
 router.post("/", validateRequest(createTimeEntrySchema), createTimeEntry);
-
-// Update time entry
 router.patch("/:id", validateRequest(updateTimeEntrySchema), updateTimeEntry);
-
-// Delete time entry
 router.delete("/:id", deleteTimeEntry);
 
 export default router;
